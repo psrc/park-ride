a38880f9d61d231248d14094898341619827a6d7
Mary Richards <mrichards@psrc.org>
2023-02-22 14:16:58 -0800

    combine and clean

--- combine.py
+++ combine.py
@@ -1,38 +1,45 @@
-from process_community_transit import process_community_transit
-from process_king_county_metro import process_king_county_metro
-from process_kitsap_transit import process_kitsap_transit
-from process_pierce_transit import process_pierce_transit
-from process_sound_transit import process_sound_transit
-
-import numpy as np
-import pandas as pd
 import os
+import pandas as pd
+import numpy as np
 
-# check working directory
-print(os.getcwd())
-os.chdir("C:\\Users\\mrichards\\Documents\\GitHub\\park-ride")
-
-# Importing external library
+# working directory
+# print(os.getcwd())
+# os.chdir("C:\\Users\\mrichards\\Documents\\GitHub\\park-ride")
 
 
-# Combine data from agencies
-output = pd.concat([process_community_transit(),
-                    process_king_county_metro(),
-                    process_kitsap_transit(),
-                    process_pierce_transit(),
-                    process_sound_transit()])
+def process_combining_agency_data():
+    """Process 2022 park & ride data from PSRC transit agencies."""
 
-# output.head(3)
+    print('Begin processing combining and cleaning park & ride data.')
 
-# Find any overlapping entries, prioritize agencies before Sount Transit
-output2 = output.sort_values(by=['name'])
+    from process_community_transit import process_community_transit
+    from process_king_county_metro import process_king_county_metro
+    from process_kitsap_transit import process_kitsap_transit
+    from process_pierce_transit import process_pierce_transit
+    from process_sound_transit import process_sound_transit
 
-output2['name'] = output2['name'].map(lambda x: x.strip())
+    # Combine data from agencies
+    combined = pd.concat([process_community_transit(),
+                          process_king_county_metro(),
+                          process_kitsap_transit(),
+                          process_pierce_transit(),
+                          process_sound_transit()])
 
-# Selecting duplicate rows
-duplicate = output2[output2.duplicated('name', keep=False)]
+    # Sorting data and removing leading/trailing spaces
+    output = combined.sort_values(by=['name'])
+    output['name'] = output['name'].map(lambda x: x.strip())
 
-print("Duplicate Rows :")
+    # Find overlapping entries, prioritize agencies before Sount Transit
+    # Selecting duplicate rows
+    duplicate = output[output.duplicated('name', keep=False)]
 
-# Print the resultant Dataframe
-duplicate
+    # Remove Sound Transit lots within overlap dataframe
+    duplicate2 = duplicate[duplicate.agency == "Sound Transit"]
+
+    # Merge full dataset, removing Sound Tranist duplicates
+    global output2
+    output2 = pd.merge(output, duplicate2, indicator=True, how='outer').query(
+        '_merge=="left_only"').drop('_merge', axis=1)
+
+    print('Dataframe generated.')
+    # return output2
