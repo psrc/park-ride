89ccdc283c099412545c7d9054fe39d0374a54dd
grantmgibson <53488250+grantmgibson@users.noreply.github.com>
2024-05-02 15:47:03 -0700

    Update process_sound_transit.py

--- process_sound_transit.py
+++ process_sound_transit.py
@@ -2,8 +2,8 @@ import os
 import pandas as pd
 import numpy as np
 
-def process_sound_transit():
-    """Pulls 2022 Sound Transit files from folder and combines into one table.
+def process_sound_transit(year):
+    """Pulls Sound Transit files from folder using current project year and combines into one table.
     
     Each monthly file contains observed parking counts for Tuesday-Thursday.
     Daily counts are averaged per month, then months are combined and averaged
@@ -13,26 +13,37 @@ def process_sound_transit():
     print('Begin processing monthly Sound Transit park & ride data.')
     
     # Assign path to agency in project folder; create list of files in folder
-    file_path = 'J:/Projects/Surveys/ParkRide/Data/2022/Sound Transit/XLSX/'
+    file_path = 'J:/Projects/Surveys/ParkRide/Data/' + str(year) + '/Sound Transit/XLSX/'
     dir_list = os.listdir(file_path)
     
     # Loop through monthly files in folder and join months one by one
     processed = pd.DataFrame()
     for i in dir_list:
-        if 'oct' in i.lower():
-            df = pd.read_excel(io=file_path + i, header=1, skipfooter=3)
+        if 'july' in i.lower():
+            df = pd.read_excel(io=file_path + i, skipfooter=2)
+        elif 'feb' in i.lower():
+            df = pd.read_excel(io=file_path + i, skiprows=[1], skipfooter=3)
         else:
             df = pd.read_excel(io=file_path + i, skipfooter=4)
             
         month = list(df.columns.values)
         month = month[2][:3].lower()
+        
+        if month in ['jul', 'jun']:
+            # Remove 'Mon Year ' from column names; replace spaces with _ in names and lowercase
+            df.columns = df.columns.str.replace(r'(\w{3}\s+\d{4}\s+)', '', regex=True).str.strip()
+            df.columns = df.columns.str.replace(' ', '_', regex=True).str.lower()
+            
+            # Rename 'usage' column
+            df.rename({'usage': 'occupancy'}, axis=1, inplace=True)
+        else:
+            # Remove 'Mon Year ' and 'Usage ' from column names; replace spaces with _ in names and lowercase
+            df.columns = df.columns.str.replace(r'(\w{3}\s+\d{4}\s+)', '', regex=True).str.replace(r'Usage\s', '', regex=True)
+            df.columns = df.columns.str.replace(' ', '_', regex=True).str.lower()
 
-        # Remove 'Mon Year ' and 'Usage ' from column names; replace spaces with _ in names and lowercase
-        df.columns = df.columns.str.replace(r'(\w{3}\s\d{4}\s+)', '', regex=True).str.replace(r'Usage\s', '', regex=True)
-        df.columns = df.columns.str.replace(' ', '_', regex=True).str.lower()
+            # Create column of avg occupied spaces; recode service_type; add agency column
+            df = df.assign(occupancy = df.loc[:, ['tuesday', 'wednesday', 'thursday']].mean(axis=1).round(0))
 
-        # Create column of avg occupied spaces; recode service_type; add agency column
-        df = df.assign(occupancy = df.loc[:, ['tuesday', 'wednesday', 'thursday']].mean(axis=1).round(0))
         df['service_type'] = np.where(df['service_type'].str.contains('^leas', case=False, regex=True), 'leased', 'permanent')
 
         # Subset df to only needed columns
@@ -48,20 +59,20 @@ def process_sound_transit():
         if len(processed.columns) == 0:
             processed = df.copy(deep=True)
         else:
-            processed = pd.merge(processed, df, how='left', on=['name', 'owner_status', 'address'])
+            processed = pd.merge(processed, df, how='outer', on=['name', 'owner_status', 'address'])
         
         print('Done processing {} data.'.format(month))
         
     # Average monthly usage to annual
-    processed = processed.assign(occupancy = processed.loc[:, ['o_jan', 'o_feb', 'o_mar',
-                                                               'o_apr', 'o_may', 'o_jun',
-                                                               'o_jul', 'o_aug', 'o_sep',
-                                                               'o_oct', 'o_nov', 'o_dec']].mean(axis=1).round(0))
-    
     processed = processed.assign(capacity = processed.loc[:, ['c_jan', 'c_feb', 'c_mar',
                                                               'c_apr', 'c_may', 'c_jun',
                                                               'c_jul', 'c_aug', 'c_sep',
                                                               'c_oct', 'c_nov', 'c_dec']].mean(axis=1).round(0))
+    
+    processed = processed.assign(occupancy = processed.loc[:, ['o_jan', 'o_feb', 'o_mar',
+                                                               'o_apr', 'o_may', 'o_jun',
+                                                               'o_jul', 'o_aug', 'o_sep',
+                                                               'o_oct', 'o_nov', 'o_dec']].mean(axis=1).round(0))
     
     # Subset dataframe to needed columns for output; insert notes, agency name
     processed = processed.loc[:, ['name', 'owner_status', 'address', 'capacity', 'occupancy']]
